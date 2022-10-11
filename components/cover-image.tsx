0ff4a86939bd0e001d89f72bd94640765ffd8213
Shunta <exec.apea@gmail.com>
2022-10-12 00:55:51 +0900

    UPDATE: progress in replace tailwindcss with chakra

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames'
 import Link from 'next/link'
+import { Image } from '@chakra-ui/react'
 
 type Props = {
   title: string
@@ -9,13 +10,10 @@ type Props = {
 
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
-    <img
-      src={src}
-      alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm', {
-        'hover:shadow-lg transition-shadow duration-200': slug,
-      })}
-    />
+      <Image src={src}
+             alt={`Cover Image for ${title}`}
+             boxSize='150px'
+      />
   )
   return (
     <div className="sm:mx-0">
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,10 @@
 import { CMS_NAME } from '../lib/constants'
+import { Heading } from '@chakra-ui/react'
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
+        <Heading>わたしのぶろぐ</Heading>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{' '}
         <a
