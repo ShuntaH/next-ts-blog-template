ab8dd78a195865d42b539eba943f439fb8f86b67
Shunta <exec.apea@gmail.com>
2022-10-29 15:02:29 +0900

    FIX: hide eslint error of files under app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-
-export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}: {g
-    children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-}
+export {}
+// import React from 'react';
+//
+// export default function RootLayout({
+//   // Layouts must accept a children prop.
+//   // This will be populated with nested layouts or pages
+//   children,
+// }: {g
+//     children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <title>Next.js</title>
+//       </head>
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
 
 import { AppProps } from 'next/app'
 // import { ChakraProvider } from "@chakra-ui/react";
