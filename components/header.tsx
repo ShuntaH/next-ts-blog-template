36dcde97cb668fd796cffec032a0960b4c0b0f1f
Shunta <exec.apea@gmail.com>
2023-01-09 13:02:43 +0900

    wip

--- components/header.tsx
+++ components/header.tsx
@@ -1,34 +1,34 @@
-import { Flex, Heading, HStack, Link } from '@chakra-ui/react'
-import { BLOG_NAME } from "../lib/constants";
+import {Flex, Heading, HStack, Link} from '@chakra-ui/react'
+import {BLOG_NAME} from "../lib/constants";
 
 const Header = () => {
-  return (
-    <Flex
-      w={'100%'}
-      h={'100px'}
-      maxWidth={'1200px'}
-      marginInline={"auto"}
-      px={4}
-      justifyContent={'space-between'}
-      alignItems={"center"}
-      letterSpacing={'0.02em'}
-    >
-      <Heading as='h1' size='md' noOfLines={1}>
-        <Link href='/'>
-          {BLOG_NAME}
-        </Link>
-      </Heading>
+    return (
+        <Flex
+            w={'100%'}
+            h={'100px'}
+            maxWidth={'1200px'}
+            marginInline={"auto"}
+            px={4}
+            justifyContent={'space-between'}
+            alignItems={"center"}
+            letterSpacing={'0.02em'}
+        >
+            <Heading as='h1' size='md' noOfLines={1}>
+                <Link href='/'>
+                    {BLOG_NAME}
+                </Link>
+            </Heading>
 
-      <HStack spacing='24px'>
-        <Link href='/terms' fontWeight={'medium'}>
-          Terms
-        </Link>
-        <Link href='/about' fontWeight={'medium'}>
-          About
-        </Link>
-      </HStack>
-    </Flex>
-  );
+            <HStack spacing='24px'>
+                <Link href='/terms' fontWeight={'medium'}>
+                    Terms
+                </Link>
+                <Link href='/about' fontWeight={'medium'}>
+                    About
+                </Link>
+            </HStack>
+        </Flex>
+    );
 }
 
 export default Header
