80aa03b2df3868e9659dccfb0e23c1576030dec6
Shunta <exec.apea@gmail.com>
2023-01-22 17:56:26 +0900

    UPDATE: footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,21 @@
-import {Flex, Text} from "@chakra-ui/react";
-import {BLOG_NAME} from "../lib/constants";
+import { Box, Text } from "@chakra-ui/react";
+import { BLOG_NAME, STYLES } from "../lib/constants";
 
 const Footer = () => {
-    return (
-        <Flex
-            w={'100%'}
-            h={'60px'}
-            alignItems={'center'}
-            justifyContent={'center'}
-            letterSpacing={'0.02em'}
-        >
-            <Text fontWeight={'medium'}>
-                ©︎ {new Date().getFullYear()} {BLOG_NAME}
-            </Text>
-        </Flex>
-    )
+  return (
+    <Box
+      as={"footer"}
+      maxWidth={`calc(${STYLES.mainWidth} + ${STYLES.gap} * 2)`}
+      margin={"auto"}
+      textAlign={"center"}
+      lineHeight={"24px"}
+      padding={`calc(${STYLES.footerHeight} - ${STYLES.gap} / 2) ${STYLES.gap}`}
+    >
+      <Text fontWeight={'medium'} fontSize={"sm"}>
+        ©︎ {new Date().getFullYear()} {BLOG_NAME}
+      </Text>
+    </Box>
+  )
 }
 
 export default Footer
