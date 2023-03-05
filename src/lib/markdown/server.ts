e95359cc72ee31a39137eaedffc7cacbea60ceb1
Shunta <exec.apea@gmail.com>
2023-03-05 16:59:34 +0900

    ADD: rehype slug plugin to add id attr to heading elms

--- src/lib/markdown/server.ts
+++ src/lib/markdown/server.ts
@@ -7,6 +7,7 @@ import rehypeStringify from "rehype-stringify";
 import remarkUnwrapImages from "remark-unwrap-images";
 import remarkToc from "remark-toc";
 import { TOC_HEADING } from "lib/constants";
+import rehypeSlug from "rehype-slug";
 
 
 export async function markdownToHtml(markdown: string): Promise<string> {
@@ -17,7 +18,7 @@ export async function markdownToHtml(markdown: string): Promise<string> {
     .use(remarkPrism, {}) // code highlight
     .use(remarkUnwrapImages) // md の中の img タグを p タグで囲むのをHTMLに違反しないためにやめる
     .use(remarkRehype, { allowDangerousHtml: true })// markdownの中の img タグを img タグのままにする
-    // .use(rehypeFormat) // 整形されていないHTMLを空白や改行を消して整える。
+    .use(rehypeSlug) // h1, h2, h3 に id を付与する
     .use(rehypeStringify, { allowDangerousHtml: true }) // htmlタグをそのまま出力する
     .processSync(markdown).toString()
 }
