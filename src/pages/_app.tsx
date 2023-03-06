445ad562b69c3771170872b610a3d8eabe90a74e
Shunta <exec.apea@gmail.com>
2023-03-06 22:07:42 +0900

    UPDATE: install next-seo and delete _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,18 +7,11 @@ import BlurBackground from "components/blur-background";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // 本来はここに Layout のコンポーネントを置きたい。
-    // 全文検索するクラスインスタンスを全ページで使うので、
-    // 事前に1回だけ作って useContext で管理したい。
-    // インスタンスを作るためには全ての記事を取得している必要がある。
-    // 記事を取得するのはサーバーサイドで行われる getStaticProps。
-    // しかし getStaticProps は _app.tsx では書けない。
+    // 検索データを共通レイアウトの Layout に渡すために
+    // getServersideProps 使うために pages に書いている。
     // app.tsx でも getInitialProps を使えばサーバーサイドの処理になるが、
     // SSG では画像の最適化がされなくなるので、使わない。
     // ref: https://nextjs.org/docs/advanced-features/custom-app
-    // pages のコンポーネントなら getStaticProps が呼べるので、記事が取得できる。
-    // pages のコンポーネントの一番最初に毎回 layout を置いている。
-    // layout の中に Context.Provider を起き、
-    // それで fuse インスタンス を任意の場所で受け取る。
     <ChakraProvider theme={theme}>
       <BlurBackground/>
       <Component {...pageProps} />
