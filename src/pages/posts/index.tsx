94e1f8a808f376c33afb6b1c27fbff8a67478d91
Shunta <exec.apea@gmail.com>
2023-03-12 17:22:44 +0900

    ADD: add redirect handler to posts/index.tsx

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,16 @@
-import { useEffect } from "react";
+import { GetStaticPropsResult } from "next";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
+
+async function getStaticProps(): Promise<GetStaticPropsResult<undefined>> {
+  return {
+    redirect: {
+      destination: '/pages/1',
+      statusCode: 308
+    }
+  }
+}
 
 export default function PaginatedPage() {
   const router = useRouter()
