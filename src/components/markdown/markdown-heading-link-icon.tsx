8bf7e9948ab91f4fff9d211fe9d92716200b2e2d
Shunta <exec.apea@gmail.com>
2023-03-19 02:54:36 +0900

    CHORE: remove consolelog

--- src/components/markdown/markdown-heading-link-icon.tsx
+++ src/components/markdown/markdown-heading-link-icon.tsx
@@ -3,7 +3,6 @@ import { Text, TextProps } from '@chakra-ui/react'
 import { HEADING_LINK_ICON_CLASSNAME } from "../../lib/constants";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import ChakraFontAwesomeIcon from "../foundations/chakra-font-awesome-icon";
-import { devLog } from "../../lib/helpers";
 
 /**
  * Headingにリンク要素をrehypeAutolinkHeadingsで追加している。そこにspan要素が含まれているので、
@@ -12,7 +11,6 @@ import { devLog } from "../../lib/helpers";
  * @param props
  */
 function MarkdownHeadingLinkIcon (props: TextProps) {
-  devLog(['MarkdownHeadingLinkIcon', props])
     return (
       props.className?.includes(HEADING_LINK_ICON_CLASSNAME) ?
         <ChakraFontAwesomeIcon icon={faLink} width={4} display={"inline-block"} marginInlineStart={2}/>
