bc4c72a88ee61b7a8542315ecfebe55b3dd4b8b5
Shunta <exec.apea@gmail.com>
2023-03-27 01:43:42 +0900

    UPDATE: enable to open the modal by clicking input anywhere

--- src/components/search/header/search-form-control-in-header.tsx
+++ src/components/search/header/search-form-control-in-header.tsx
@@ -8,7 +8,7 @@ import {
   InputRightElement,
   Kbd
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { FormEventHandler, useCallback } from 'react'
 import { STYLES } from 'lib/constants'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import ChakraFontAwesomeIcon from 'components/foundations/chakra-font-awesome-icon'
@@ -28,6 +28,13 @@ function SearchFormControlInHeader({ formControlProps }: Props) {
   const { onOpen } = useSearchModalDisclosure()
   const {actionKey} = useKeyboard()
 
+  const handleInput: FormEventHandler = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      e.preventDefault()
+    }
+, [])
+
+
   return (
     <FormControl
       position={'relative'}
@@ -35,11 +42,14 @@ function SearchFormControlInHeader({ formControlProps }: Props) {
       {...formControlProps}
     >
       {/* 入力欄と虫眼鏡アイコンで1つの検索入力欄としてグループを作る */}
-      <InputGroup size='md'>
-        <InputLeftElement>
+      <InputGroup
+        size='md'
+        position={"relative"}
+        onClick={onOpen}
+      >
+        <InputLeftElement zIndex={1}>
           <ChakraFontAwesomeIcon
             icon={faMagnifyingGlass}
-            onClick={onOpen}
             display={'inline'}
             width={4}
             opacity={0.4}
@@ -48,9 +58,14 @@ function SearchFormControlInHeader({ formControlProps }: Props) {
         <Input
           type='text'
           focusBorderColor={STYLES.colorLight}
-          onClick={onOpen}
+          onInput={handleInput}
+          value={''}
         />
-        <InputRightElement width={36} whiteSpace={"nowrap"}>
+        <InputRightElement
+          width={36}
+          whiteSpace={"nowrap"}
+          zIndex={1}
+        >
             <Flex
               h={"full"}
               alignItems={"center"}
