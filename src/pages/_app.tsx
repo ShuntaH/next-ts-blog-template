d6ed248fd51fb4bc51f4fe8fd740bab649d134a0
Shunta <exec.apea@gmail.com>
2023-03-27 19:13:16 +0900

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,8 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { DefaultSeo } from 'next-seo'
-import { DEFAULT_SEO } from 'lib/constants'
 import React from 'react'
 import theme from 'theme'
-import BlurBackground from 'components/blur-background'
 import { useGtm } from "../hooks/useGtm";
-import GtmScript from "../components/gtm-script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useGtm();
@@ -17,9 +13,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   // SSG では画像の最適化がされなくなるので、使わない。
   // ref: https://nextjs.org/docs/advanced-features/custom-app
   <ChakraProvider theme={theme}>
-    <GtmScript />
-    <DefaultSeo {...DEFAULT_SEO}/>
-    <BlurBackground/>
     <Component {...pageProps} />
   </ChakraProvider>
 )
