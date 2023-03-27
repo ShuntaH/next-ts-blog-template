ba306dd6cd0a98a7561a30a42dc69a1ca8fa56f2
Shunta <exec.apea@gmail.com>
2023-03-27 19:24:13 +0900

    wip

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -2,20 +2,35 @@ import Fuse from 'fuse.js'
 import { FilteredPost } from 'interfaces/post'
 import React from 'react'
 import Header from 'components/header'
+import { Box } from '@chakra-ui/react'
+import { STYLES } from 'lib/constants'
 import Footer from 'components/footer'
 import { FuseProvider } from 'contexts/fuseContext'
 
 interface Props {
   fuse: Fuse<FilteredPost>
+  children: React.ReactNode
 }
 
-function Layout ({ fuse }: Props) {
+function Layout ({ fuse, children }: Props) {
   return (
     // fuse をサーバーサイドで作成するために getStaticProps に書く。
     // そのために _app.tsx には書かない。
     <FuseProvider fuse={fuse}>
       <Header/>
-
+      <Box
+        as={'section'}
+        position={'relative'}
+        display={'block'}
+        maxWidth={STYLES.contentMaxWidth}
+        minHeight={`calc(100vh - ${STYLES.headerHeight} - ${STYLES.footerHeight})`}
+        marginX={'auto'}
+        marginBottom={20}
+        marginTop={72}
+        padding={`${STYLES.gap}`}
+      >
+        {children}
+      </Box>
       <Footer/>
     </FuseProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { getAllPosts, getHtmlContentPosts, getPagination, getSortedPosts } from 
 import { FilteredPosts, Posts } from 'interfaces/post'
 import { Pagination } from 'interfaces/pagination'
 import Layout from 'components/layouts/layout'
-import PostList from 'components/post/postList/post-list'
 import { useSetupFuse } from 'hooks/useFuse'
 import { getFilteredPosts } from 'lib/api/filterPost'
 import { GetStaticPropsResult } from 'next'
@@ -32,14 +31,13 @@ export async function getStaticProps (): Promise<GetStaticPropsResult<Props>> {
   }
 }
 
-export default function Index ({ pagination, filteredPosts }: Props) {
+export default function Index ({ filteredPosts }: Props) {
   const fuse = useSetupFuse(filteredPosts)
 
   // このページは top page なので デフォルト設定があるため、
   // seo は設定は不要。
   return (
     <Layout fuse={fuse}>
-      <PostList pagination={pagination}/>
     </Layout>
   )
 }
