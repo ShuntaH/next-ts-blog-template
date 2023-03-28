9f7c98ed24be72e34d59b630808b3b703a9c9a32
Shunta <exec.apea@gmail.com>
2023-03-28 22:38:49 +0900

    wip

--- src/components/common/date-formatter.tsx
+++ src/components/common/date-formatter.tsx
@@ -1,13 +1,20 @@
 import { format, parseISO } from 'date-fns'
-import { useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
 interface Props {
   dateString: string
 }
 
 function DateFormatter ({ dateString }: Props) {
-  const date = useMemo(
-    () => parseISO(dateString),
+  const [date, setDate] = useState(new Date())
+
+  useEffect(
+    // デプロイ先のサーバーのタイムゾーンが異なる場合、
+    // サーバーサイドレンダリング時とクライアントサイドレンダリング時で
+    // 日付がずれるのでハイドレーションエラーが発生する。
+    () => {
+      setDate(parseISO(dateString))
+    },
     [dateString]
   )
   return (
