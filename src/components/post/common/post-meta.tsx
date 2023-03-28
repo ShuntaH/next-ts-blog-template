3b9502db784a092b8fee092788c61055282742bc
Shunta <exec.apea@gmail.com>
2023-03-28 21:52:40 +0900

    wip

--- src/components/post/common/post-meta.tsx
+++ src/components/post/common/post-meta.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Box, BoxProps, TagLabelProps } from '@chakra-ui/react'
-import PostDate from 'components/post/common/post-date'
+import { Box, BoxProps, Flex, TagLabelProps } from '@chakra-ui/react'
+import PostTime from 'components/post/common/post-time'
+import PostTags from 'components/post/common/post-tags'
 import { TagLinkProps } from 'interfaces/foundation'
 import { STYLES } from 'lib/constants'
 
@@ -30,11 +31,19 @@ function PostMeta ({
       color={STYLES.textColorDark}
       {...boxProps}
     >
-      <PostDate
-        publishedAt={publishedAt}
-        updatedAt={updatedAt}
-        textProps={{ marginBottom: { base: 0.5 }, paddingRight: 2 }}
-      />
+      <Flex alignItems={'center'} flexWrap={'wrap'}>
+        <PostTime time={time}/>
+        <PostTags
+          tags={tags}
+          stackProps={{
+            spacing: 2,
+            paddingLeft: { base: 0, md: 0.5 },
+            marginBlockStart: { base: 1, md: 0 }
+          }}
+          tagLinkProps={tagLinkProps}
+          tagLabelProps={tagLabelProps}
+        />
+      </Flex>
     </Box>
   )
 }
