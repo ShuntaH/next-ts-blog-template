50a00952a04d9355e385e353fecc7b8e6926ee91
Shunta <exec.apea@gmail.com>
2023-03-28 22:06:29 +0900

    wip

--- src/components/post/common/post-date.tsx
+++ src/components/post/common/post-date.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Box, Text, TextProps } from '@chakra-ui/react'
 import DateFormatter from 'components/common/date-formatter'
+import ChakraFontAwesomeIcon from 'components/foundations/chakra-font-awesome-icon'
+import { faFeather } from '@fortawesome/free-solid-svg-icons'
+import { STYLES } from 'lib/constants'
 
 interface Props {
   publishedAt: string
@@ -11,6 +14,14 @@ interface Props {
 function PostDate({ publishedAt, updatedAt, textProps }: Props) {
   return (
     <Box letterSpacing={'0.03em'} {...textProps}>
+      <ChakraFontAwesomeIcon
+        display={'inline'}
+        paddingTop={0.5}
+        paddingRight={1}
+        icon={faFeather}
+        color={STYLES.color}
+        width={4}
+      />
       {
         publishedAt === updatedAt ?
           null :
