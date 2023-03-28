4f7432ed507fb5a20c00c1b64db464a3595a72ee
Shunta <exec.apea@gmail.com>
2023-03-28 23:48:10 +0900

    wip

--- src/components/common/date-formatter.tsx
+++ src/components/common/date-formatter.tsx
@@ -1,26 +1,31 @@
 import { format, parseISO } from 'date-fns'
-import { useEffect, useState } from 'react'
+import { ja } from 'date-fns/locale'
 
 interface Props {
   dateString: string
 }
 
 function DateFormatter({ dateString }: Props) {
-  const [ date, setDate ] = useState(new Date())
-
-  useEffect(
-    // デプロイ先のサーバーのタイムゾーンが異なる場合、
-    // サーバーサイドレンダリング時とクライアントサイドレンダリング時で
-    // 日付がずれるのでハイドレーションエラーが発生する。
-    // そのため、useEffectを使ってクライアントサイドレンダリング時に date を更新する。
-    () => {
-      setDate(parseISO(dateString))
-    },
-    [ dateString ]
-  )
+  // const [ date, setDate ] = useState(new Date())
+  //
+  // useEffect(
+  //   // デプロイ先のサーバーのタイムゾーンが異なる場合、
+  //   // サーバーサイドレンダリング時とクライアントサイドレンダリング時で
+  //   // 日付がずれるのでハイドレーションエラーが発生する。
+  //   // そのため、useEffectを使ってクライアントサイドレンダリング時に date を更新する。
+  //   () => {
+  //     setDate(parseISO(dateString))
+  //   },
+  //   [ dateString ]
+  // )
   return (
     <time dateTime={dateString}>
-      {format(date, 'LLLL	d, yyyy')}
+      {
+        format(
+          parseISO(dateString),
+          'LLLL	d, yyyy',
+          { locale: ja })
+      }
     </time>
   )
 }
